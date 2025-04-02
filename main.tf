d0f206937b198126197741478144823fe8e2d6f5
abhishek88roshan <abhishek88roshan@yahoo.com>
2025-04-02 19:37:49 +0530

    updated main

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ locals {
 data "aws_ssm_parameter" "this" {
   cou
 
-  name = var.ami_ssm_parameter
+  name = v
 }
 soidhidsfighds
 ################################################################################
@@ -20,7 +20,7 @@ resource "aws_instance" "this" {
   count = local.create && !var.ignore_ami_changes && !var.create_spot_instance ? 1 : 0
 
   ami                  = try(coalesce(var.ami, nonsensitive(data.aws_ssm_parameter.this[0].value)), null)
-  instance_type        = var.instance_type
+  instance_type        = 
   cpu_core_count       = var.cpu_core_count
   cpu_threads_per_core = var.cpu_threads_per_core
   hibernation          = var.hibernation
